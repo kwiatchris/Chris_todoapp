45b34ada5a03a813f99f2a940fcf7cfd4002c8b7
unaiurkiola <unaiurkiola@gmail.com>
2015-09-22 16:29:42 +0200

    first commit

---
+++ formulario.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>forms</title>
+</head>
+<body>
+	<h1>Chris y Ierai to_do tarea</h1>
+
+<form action="to_do_create.php" method="post">
+Name: <input type="text" name="name"><br>
+login: <input type="text" name="login"><br>
+password: <input type="text" name="password"><br>
+<input type="submit">
+</form>
+
+</form>
+<form action="formulario.php">
+	<input type="submit" value="crear usuario">
+</form>
+</body>
+</html>
