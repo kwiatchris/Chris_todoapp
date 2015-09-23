fe7bd8736374abd4bd7250474a75cbea6070d35f
kwiatchris <kwiatchris@gmail.com>
2015-09-23 12:24:50 +0200

    desde zubiri

--- to_do_listas.php
+++ to_do_listas.php
@@ -4,7 +4,7 @@ session_start();
 echo $_SESSION['login_user'];
 echo $_SESSION['id_usuario'];
 $id_usu_sesion=$_SESSION['id_usuario'];
-echo $id_usu_sesion;
+//echo $id_usu_sesion;
 $pdo=new PDO('mysql:host=localhost;dbname=TO_DO_PHP','root','internet80');
 if(!$pdo){
 	die('could not connect' . PDO_error());
@@ -16,5 +16,14 @@ $statement=$pdo->query("SELECT * FROM  `listas` WHERE  id_usuario =  '$id_usu_se
 $statement->execute();
 $result= $statement->fetchAll(PDO::FETCH_ASSOC);
 print_r($result);
-echo "mis listas";
+echo "<br>";
+//echo "mis listas";
+echo "<br>";
+foreach ($result as $res){
+        echo'<tr>'; 
+        echo'<td>' ."usuario: ". $res['id_usuario']."</td>";echo "<br>";
+        echo'<td>' ."nombre de la lista : ". $res['nombre'].'</td>';echo "<br>";
+        echo'<td>' . "la lista creada dia ". $res['fecha_creacion'].'</td>';echo "<br>";
+        echo'<tr>';
+    }
 ?>
