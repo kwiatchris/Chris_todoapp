c3aed3f41d6bdf41ae21a7b821a29d9fcb902607
kwiatchris <kwiatchris@gmail.com>
2015-09-26 15:18:03 +0200

    try catch

--- to_do_create.php
+++ to_do_create.php
@@ -2,41 +2,38 @@
 
 <?php
 //PDO + mySQL
-
-$pdo=new PDO('mysql:host=localhost;dbname=TO_DO_PHP','root','internet80');
-if(!$pdo){
-	die('could not connect' . PDO_error());
-}
+try{	$pdo=new PDO('mysql:host=localhost;dbname=TO_DO_PHP','root','internet80');
+			if(!$pdo){
+			die('could not connect' . PDO_error());
+	}
 //var_dump($_POST);
 extract($_POST);
 //$value = mysql_real_escape_string($_POST['login']);
 $statement=$pdo->query("SELECT * FROM `usuarios` WHERE login='$login'");
 //var_dump($_POST);
 //$newlogin=$_POST['login'];
-echo $login;echo "<br>";
+//echo $login;echo "<br>";
 //echo $login;
-
-
 	//$statement->bindParam(':login',$login);
 	$statement->execute();
 	$result=$statement->fetchAll(PDO::FETCH_ASSOC);
-	print_r($result);
-	//$fila=mysql_fetch_array($result);
-	echo $fila['login'];
+	if($statement){
+			
+			//	header('Refresh:1;URL=http://localhost/Aitor/TO_DO_/TO_DO_GIT/Chris_todoapp/to_do_crearusuario.php');
+	
+	//print_r($result);//$fila=mysql_fetch_array($result);echo $fila['login'];
 	echo "<br>";
 	//echo $name;
 	echo "<br>";
-		//echo $login;
-		//echo $password;
+		//echo $login;echo $password;
 	//echo json_encode($result);
 	//echo json_encode($result[0][login]);
 	if(empty($login)||empty($nombre)||empty($password))
-			{echo "false";
-		echo "name: ".$nombre;echo "<br>";
-		echo "login : ".$login;echo "<br>";
-		echo "password : ".$password;echo "<br>";
-				echo "falta datos";
-				echo "<script language='javascript'>";
+			{
+		//echo "name: ".$nombre;echo "<br>";
+		//echo "login : ".$login;echo "<br>";
+		//echo "password : ".$password;echo "<br>";
+		echo "<script language='javascript'>";
 		echo "alert('falta datos!!!!')";
 		echo "</script>";
 		header('Refresh:1;URL=http://localhost/Aitor/TO_DO_/TO_DO_GIT/Chris_todoapp/to_do_crearusuario.php');
@@ -48,47 +45,30 @@ echo $login;echo "<br>";
 		echo "</script>";
 		header('Refresh:1;URL=http://localhost/Aitor/TO_DO_/TO_DO_GIT/Chris_todoapp/to_do_crearusuario.php');
 		
-	} else
-			{
+	} else{
 				//$insert_usuario=$pdo->query("INSERT INTO `TO_DO_PHP`.`usuarios` (`id`, `login`, `password`, `nombre`) VALUES (NULL, '$login', '$password','$nombre')");
 				$insert_usuario=$pdo->query("INSERT INTO `TO_DO_PHP`.`usuarios` (`id_usuario`, `login`, `password`, `nombre`) VALUES ('NULL', '$login', '$password','$nombre')");
 				$insert_usuario->execute();
-				echo "<script language='javascript'>";
-		echo "alert('usuario creado !!!!')";
+					if($insert_usuario){
+		echo "<script language='javascript'>";
+		echo "alert('el usuario creado corectamente!!!!')";
 		echo "</script>";
 		header('Refresh:1;URL=http://localhost/Aitor/TO_DO_/TO_DO_GIT/Chris_todoapp/to_do_login.html');
+		}else{
+		echo "<script language='javascript'>";
+		echo "alert('fallo creando el usuario!!!!')";
+		echo "</script>";
 			}
-			
-		//$fila=mysqli_fetch_array($result);
-		//echo $fila['login'];
-	
-	//header("Location:to_do_create.html");
-	
-	/*for(i=0;i<100;i++){
-		if(($result[i][login])==$login){
-			echo ("existe");
-		}else echo ("no existe");
+				
 
+			}
+}else{
+	throw new Exception('unable to connect');
 	}
-*/
-/*echo $userid;
-//$stmt = $pdo->prepare('SELECT * FROM users WHERE userid = :userid');
-// $userid = filter_input(INPUT_GET, 'userid', FILTER_SANITIZE_NUMBER_INT); // <-- filter your data first (see [Data Filtering](#data_filtering)), especially important for INSERT, UPDATE, etc.
-$userid = filter_input(INPUT_GET, "userid", FILTER_VALIDATE_INT);
-// var_dump($userid);
-echo $userid;
-if ($userid === false) {
-echo "Error";
-} else {
-$stmt->bindParam(':userid', $userid, PDO::PARAM_INT); // <-- Automatically sanitized for SQL by PDO
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($result);
-
+}catch(Exception $e){
+	echo $e->getMessage();
 }
-//document.write("<br>");
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($result);
-*/
+		
+
 ?>
 
--- to_do_listas.php
+++ to_do_listas.php
@@ -1,48 +1,35 @@
 <?php
 session_start();
-//include('to_do.php');
 //echo $_SESSION['login_user'];
 //echo $_SESSION['id_usuario'];
 $id_usu_sesion=$_SESSION['id_usuario'];
 //echo $id_usu_sesion;
-$pdo=new PDO('mysql:host=localhost;dbname=TO_DO_PHP','root','internet80');
-if(!$pdo){
-	die('could not connect' . PDO_error());
-}
-$statement=$pdo->query("SELECT * FROM  `listas` WHERE  id_usuario =  '$id_usu_sesion'");
-$statement_tarea=$pdo->query("SELECT * FROM  `tareas` WHERE  `id_usuario` ='$id_usu_sesion'");
-$statement_tarea_con_listas=$pdo->query("SELECT  `id_usuario`, `id_lista`, `texto` FROM `tareas` WHERE `id_usuario` ='$id_usu_sesion'&&'id_lista='$res('id_lista')'");
-//echo $_SESSION['id_usuario'];echo "<br>";
-//var_dump($_POST);
-//$statement->execute();
-//$result= $statement->fetchAll(PDO::FETCH_ASSOC);
-//********************************************
-// egual mal escrita la select de tarea con listas
-$statement_tarea_con_listas->execute();
-$result_listas=$statement_tarea_con_listas->fetchAll(PDO::FETCH_ASSOC);
-print_r($result_listas);
-
-print_r($result);
-//echo "<br>";
+try {
+	$pdo=new PDO('mysql:host=localhost;dbname=TO_DO_PHP','root','internet80');
+		if(!$pdo){
+				die('could not connect' . PDO_error());
+					}
+	$statement=$pdo->query("SELECT * FROM  `listas` WHERE  id_usuario =  '$id_usu_sesion'");
+	//$statement_tarea=$pdo->query("SELECT * FROM  `tareas` WHERE  `id_usuario` ='$id_usu_sesion'");
+	//echo $_SESSION['id_usuario'];echo "<br>";
+	$statement->execute();
+	$result= $statement->fetchAll(PDO::FETCH_ASSOC);
+	}catch(PDOExeption $e){
+		echo $e->getMessage();
+	}
+//print_r($result);echo "<br>";
 $id_lista=$result[0]['id_lista'];//pasamos numero de la lista desde la querry $statement
-//echo "numero de la lista: " .$id_lista;
 echo "<br>";
 foreach ($result as $res){//recoremos el resultado por filas adecuados[''] desde query $statement
         echo'<tr>';
-        echo'<td>' ."lista: ". $res['id_lista']."</td>";echo "<br>"; 
-        echo'<td>' ."usuario: ". $res['id_usuario']."</td>";echo "<br>";
+        //echo'<td>' ."lista: ". $res['id_lista']."</td>";echo "<br>"; 
+        //echo'<td>' ."usuario: ". $res['id_usuario']."</td>";echo "<br>";
         echo'<td>' ."nombre de la lista : ". $res['nombre'].'</td>';echo "<br>";
-        echo'<td>' . "la lista creada dia ". $res['fecha_creacion'].'</td>';echo "<br>";
+       // echo'<td>' . "la lista creada dia ". $res['fecha_creacion'].'</td>';echo "<br>";
         echo'<tr>';
-        /*$statement_tarea->execute();//query in execucion donde buscamos las tareas del $_SESSION['id_usuario']
-$resu_tarea=$statement_tarea->fetchAll(PDO::FETCH_ASSOC);
-//print_r($resu_tarea);
-foreach ($resu_tarea as $value) {//recorremos el array q devuelto la query $statement_tarea
-	echo "tareas: ".$value['texto'];echo "<br>";
-}*/
+        echo "<br>";echo "<br>";
     }
-
-//extract($_POST);
+extract($_POST);
 //esto poner en to_do_crear _tarea con $POST
 /*if(!isset($tarea)){
 			echo "<script language='javascript'>";
@@ -53,16 +40,6 @@ elseif(!empty($tarea)){
 $statement_tarea_crear=$pdo->query("INSERT INTO  `tareas` (`id_tarea` ,`id_lista` ,`id_usuario` ,`texto`)
 VALUES ('null',  '$id_lista',  '$id_usu_sesion',  '$texto');");
 }*/
-/*if (isset($_POST['Submit1'])) {
-$username = $_POST['username'];
-if ($username == "letmein") {
-print ("Welcome back, friend!");
-}
-else {
-print ("You're not a member of this site");
-}
-}*/
-
 ?>
 <!-- formulario de la tarea -->
 <form action="" method="post">
@@ -84,24 +61,36 @@ if(isset($_POST['submit'])&&(!empty($nuevatarea))){//creamos nueva tarea
 	//echo $nuevatarea;echo "<br>";
 	$statement_tarea_crear=$pdo->query("INSERT INTO  `tareas` (`id_tarea` ,`id_lista` ,`id_usuario` ,`texto`)
 VALUES ('null',  '$id_lista',  '$id_usu_sesion',  '$nuevatarea');");//insertamos la tarea al BD
-	//$statement_tarea_crear->execute();
-	/*$statement_tarea=$pdo->query("SELECT * FROM  `tareas` WHERE  `id_usuario` ='$id_usu_sesion'");
-	$statement_tarea->execute();//query in execucion donde buscamos las tareas del $_SESSION['id_usuario']
-	$nuevatarea="";
-		$resu_tarea=$statement_tarea->fetchAll(PDO::FETCH_ASSOC);
+	//comprobamos si el INSERT se ejecuto correctamente
+	if($statement_tarea_crear){
+		echo "<script language='javascript'>";
+		echo "alert('la tarea creada corectamente!!!!')";
+		echo "</script>";
+		echo $tarea;
+	}else{
+		echo "<script language='javascript'>";
+		echo "alert('fallo creando la tarea!!!!')";
+		echo "</script>";
+	}
+	/*$resu_tarea=$statement_tarea->fetchAll(PDO::FETCH_ASSOC);**********************************
 	foreach ($resu_tarea as $value) {//recorremos el array q devuelto la query $statement_tarea
 	echo "tareas: ".$value['texto'];echo "<br>";
 }*/
-	echo $tarea;
-}
+	}
 
-//print_r(getdate());
 if(isset($_POST['submit'])&&(!empty($nuevalista))){//creamos nueva lista
-	echo $nuevalista;
-	
 	$statement_lista_crear=$pdo->query("INSERT INTO `listas`(`id_lista`, `id_usuario`, `nombre`) 
 VALUES ('null','$id_usu_sesion','$nuevalista');");//insertamos la lista al BD
-
+if($statement_lista_crear){
+		echo "<script language='javascript'>";
+		echo "alert('la lista creada corectamente!!!!')";
+		echo "</script>";
+		echo $nuevalista;
+	}else{
+		echo "<script language='javascript'>";
+		echo "alert('fallo creando la lista!!!!')";
+		echo "</script>";
+	}
 	/*//$statement_tarea_crear->execute();
 	$statement_tarea=$pdo->query("SELECT * FROM  `tareas` WHERE  `id_usuario` ='$id_usu_sesion'");
 	$statement_tarea->execute();//query in execucion donde buscamos las tareas del $_SESSION['id_usuario']
